09fb0ee7e1547f300804edec6c32d85a9a3d2b76
Shweta Padubidri <spadubid@redhat.com>
2026-03-20 14:01:01 -0400

    Add support for dynamically querying multiple tenants in trace domain
    Signed-off-by: Shweta Padubidri <spadubid@redhat.com>

--- web/src/hooks/useTroubleshootingPanel.tsx
+++ web/src/hooks/useTroubleshootingPanel.tsx
@@ -4,6 +4,8 @@ import * as React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useDispatch } from 'react-redux';
 import { openTP } from '../redux-actions';
+import { replaceTraceStore } from '../korrel8r-client';
+import { extractTraceContext, buildTraceStoreConfig } from '../utils/traceStoreUtils';
 import { useKorrel8r } from './useKorrel8r';
 
 const useTroubleshootingPanel: ExtensionHook<Array<Action>> = () => {
@@ -11,7 +13,25 @@ const useTroubleshootingPanel: ExtensionHook<Array<Action>> = () => {
   const { t } = useTranslation('plugin__troubleshooting-panel-console-plugin');
   const [perspective] = useActivePerspective();
   const dispatch = useDispatch();
-  const open = React.useCallback(() => {
+  const open = React.useCallback(async () => {
+    // Check if we're on a traces page with tenant information
+    const traceContext = extractTraceContext();
+
+    if (traceContext) {
+      try {
+        // Update korrel8r's trace store to match the selected tenant
+        const storeConfig = buildTraceStoreConfig(traceContext);
+        await replaceTraceStore(storeConfig);
+        // eslint-disable-next-line no-console
+        console.log('Trace store updated for tenant:', traceContext.tenant);
+      } catch (error) {
+        // Log error but don't block panel from opening
+        // The panel will still work with other domains even if trace store update fails
+        // eslint-disable-next-line no-console
+        console.error('Failed to update trace store:', error);
+      }
+    }
+
     dispatch(openTP());
   }, [dispatch]);
 
